767b2944e94fbd2855ce363b98ac59dcd5253490
dheidet <dimitri@36quaidesaromes.fr>
2016-02-08 12:40:40 +0100

    exercise 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # get 'coachings/ask'
+  get 'ask' => 'coachings#ask'
+
+  # get 'coachings/answer'
+  get 'answer' => 'coachings#answer'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
